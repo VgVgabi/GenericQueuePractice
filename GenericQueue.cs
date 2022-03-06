cb7e8a98b82eeda8f9c051b44d7b22d3ea402372
vr6we <vr6we@LAPTOP-RC84REDT>
2022-03-06 17:36:05 +0200

    Add project files.

---
+++ GenericQueue.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+
+namespace GenericQueuePractice
+{
+    class GenericQueue<TypeParameter>
+    {               
+        private Queue<TypeParameter> intQ = new Queue<TypeParameter>();
+        //
+        public int Count { get { return intQ.Count; } }
+        public void Enqueue(TypeParameter number)
+        {
+             intQ.Enqueue(number);
+        }
+        //
+        public object Dequeue()
+        {           
+            return intQ.Dequeue();
+        }
+        //
+        public object Peek()
+        {
+            if (intQ.Count > 0)            
+                return intQ.Peek();
+            else
+                return null;
+        }
+        //
+        public bool Contains(TypeParameter number)
+        {
+            bool searchStatus = intQ.Contains(number);
+            return searchStatus;
+        }
+        //                
+        public object GetItemsTypeName()
+        {
+            return intQ.FirstOrDefault().GetType().Name;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace GenericQueuePractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericQueue<int> numbersQ = new GenericQueue<int>();
+            Console.WriteLine($"GenericQueue API tester for {numbersQ.GetItemsTypeName()}:");
+            Console.WriteLine($"numbersQ.Count={numbersQ.Count}");
+            numbersQ.Enqueue(1);
+            numbersQ.Enqueue(2);
+            numbersQ.Enqueue(3);
+            numbersQ.Enqueue(4);
+            Console.WriteLine($"numbersQ.Count={numbersQ.Count}");
+            Console.WriteLine($"numbersQ.Contains(4)={numbersQ.Contains(4)}");
+            Console.WriteLine($"numbersQ.Contains(5)={numbersQ.Contains(5)}");
+            Console.WriteLine($"numbersQ.Peek()={numbersQ.Peek()}");
+            Console.WriteLine($"numbersQ.Dequeue()={numbersQ.Dequeue()}");
+            Console.WriteLine($"numbersQ.Dequeue()={numbersQ.Dequeue()}");
+            Console.WriteLine($"numbersQ.Dequeue()={numbersQ.Dequeue()}");
+            Console.WriteLine($"numbersQ.Dequeue()={numbersQ.Dequeue()}");
+            Console.WriteLine($"numbersQ.Count={numbersQ.Count}");
+        }
+    }
+}
